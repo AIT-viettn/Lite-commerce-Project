3ee38da773bd7bec99fd2572950341752f4f4f57
Trần Nhơn Viết <75728135+ThichTinhTam@users.noreply.github.com>
2024-03-07 21:57:51 +0700

    full DB

--- SV20T1020639.BusinessLayers/ProductDataServices.cs
+++ SV20T1020639.BusinessLayers/ProductDataServices.cs
@@ -46,10 +46,135 @@ namespace SV20T1020639.BusinessLayers
             rowCount = productDB.Count(searchValue);
             return productDB.List(page, pageSize, searchValue,categoryId,supplierId,minPrice,maxPrice).ToList();
         }
-
-        public static Product GetProduct(int productId) 
+        /// <summary>
+        /// Lấy thông tin 1 mặt hàng theo mã mặt hàng
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public static Product? GetProduct(int productId) 
         {
             return productDB.Get(productId);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int AddProduct(Product data)
+        {
+            return productDB.Add(data);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateProduct(Product data)
+        {
+            return productDB.Update(data);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public static bool DeleteProduct(int productID)
+        {
+            if (productDB.InUsed(productID))
+            {
+                return false;
+            }
+            return productDB.Delete(productID);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public static bool InUsedProduct(int productID)
+        {
+            return productDB.InUsed(productID);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public static List<ProductPhoto> ListPhotos(int productID)
+        {
+            return productDB.ListPhotos(productID).ToList();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="photoID"></param>
+        /// <returns></returns>
+        public static ProductPhoto? GetPhoto(long photoID)
+        {
+            return productDB.GetPhoto(photoID);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static long AddPhoto(ProductPhoto data)
+        {
+            return productDB.AddPhoto(data);
+        } 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="photoID"></param>
+        /// <returns></returns>
+        public static bool DeletePhoto(long photoID)
+        {
+            return productDB.DeletePhoto(photoID);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public static List<ProductAttribute> ListAttributes(int productID)
+        {
+            return productDB.ListAttributes(productID).ToList();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="attributeID"></param>
+        /// <returns></returns>
+        public static ProductAttribute? GetAttribute(int attributeID)
+        {
+            return productDB.GetAttribute(attributeID);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static long AddAttribute(ProductAttribute data)
+        {
+            return productDB.AddAtrribute(data);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateAttribute(ProductAttribute data)
+        {
+            return productDB.UpdateAtrribute(data);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="attributeID"></param>
+        /// <returns></returns>
+        public static bool DeleteAttribute(long attributeID)
+        {
+            return productDB.DeleteAtrribute(attributeID);
         }
     }
 }
